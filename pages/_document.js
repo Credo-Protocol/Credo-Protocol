a3ca6df4743a434f091040f449eb1f4969d4a686
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2025-10-14 21:47:41 +0800

    added icon for browser tab

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default function Document() {
       <Head>
         <meta name="description" content="Credo Protocol - Decentralized Trust for Capital. Identity-based lending on Moca Chain." />
         <meta name="keywords" content="Credo Protocol, DeFi, Moca Chain, Lending, Credit Score, Web3" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/credo_nobg.jpg" type="image/jpeg" />
+        <link rel="shortcut icon" href="/credo_nobg.jpg" type="image/jpeg" />
+        <link rel="apple-touch-icon" href="/credo_nobg.jpg" />
       </Head>
       <body className="antialiased">
         <Main />
